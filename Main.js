a52cca2e0f59798ed8e24a96ebd95edb8d27f498
dariu <darius_buzura@yahoo.com>
2022-03-14 14:55:55 +0100

    Ammenagement et V1

--- Main.js
+++ Main.js
@@ -6,34 +6,49 @@ const rl = readline.createInterface({
 });
 
 rl.question("Quelle est votre recherche ?\n", rech => {
-    getData("https://www.google.com/search?q="+rech);
+    getLien("https://www.google.com/search?q="+rech);
 });
 
-async function getData(url) {
+async function getLien(url) {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.setViewport({height: 800, width: 1200})
     await page.goto(url);
-    const lien = await page.evaluate(() => {
-        let slct = "div.yuRUbf>a";
-        let a = document.querySelectorAll(slct);
-        let lien = [];
-        for (let element of a) {
-            lien.push(element.href)
-        }
-        return lien
-    });
-    console.log(lien);
-    const page2 = await browser.newPage();
-    let titre = [];
-    for (let i of lien){
-        await page2.goto(i);
-        const ttl = await page2.evaluate(()=> {
-            let slct = "p";
-            let ttl = document.querySelector(slct);
-            return ttl.innerText;
-        });
-        titre.push(ttl);
+    const lien = await page.evaluate(getAllA);
+    scraping(browser,lien)
+}
+
+async function scraping(browser,lien){
+    const page = await browser.newPage();
+    let Alldata = [];
+    for (let i of lien) {
+        await page.goto(i);
+        const datas = await page.evaluate(getAllP);
+        Alldata.push(datas);
     }
-    console.log(titre);
+}
+
+function getP(){
+    let slct = "p";
+    let data = document.querySelector(slct);
+    return data.innerText;
+}
+
+function getAllP(){
+    let slct = "p";
+    let data = document.querySelectorAll(slct);
+    let datas = [];
+    for (let element of data)
+        datas.push(element.innerText)
+    return datas;
+}
+
+function getAllA(){
+    let slct = "div.yuRUbf>a";
+    let a = document.querySelectorAll(slct);
+    let lien = [];
+    for (let element of a) {
+        lien.push(element.href)
+    }
+    return lien
 }
