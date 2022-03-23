7c9fc38881128ddadac791f91e134be7a98fb3a1
dariu <darius_buzura@yahoo.com>
2022-03-23 11:10:13 +0100

    Changement site (html et css)

---
+++ Site/Index.js
@@ -0,0 +1,7 @@
+let slct = document.querySelector('index')
+let tux = document.getElementById('recherche')
+
+document.addEventListener('keydown', function (event){
+   console.log("Touche enfoncé : ",event.key);
+
+});
