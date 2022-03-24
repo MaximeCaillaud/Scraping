321c01c1576a8f24ed477f85980b5de4993ea659
Marks132 <maxime.caillaud93@gmail.com>
2022-03-24 23:52:49 +0100

    Version Final

--- Main.js
+++ Main.js
@@ -1,14 +1,11 @@
 const puppeteer = require('puppeteer');
-const readline = require('readline');
 const express = require('express')
 const app = express()
 const lda = require('lda');
 const {request, response} = require("express");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+
 // https://www.google.com/search?q=
+
 app.get('/',(req,res) =>{
     res.sendFile('/Users/33699/WebstormProjects/Scraping/Site/index.html');
 })
@@ -26,14 +23,13 @@ async function getLien(url,rech) {
     await page.setViewport({height: 800, width: 1200})
     await page.goto(url);
     const lien = await page.evaluate(getAllA);
-    let a = await scraping(browser,lien,rech);
-    return a;
+    return await scraping(browser, lien, rech);
 }
 
 async function scraping(browser,lien,rech){
     const page = await browser.newPage();
     let Alldata = [];
-    let body = "";
+    let body = ""
     for (let i of lien) {
         await page.goto(i);
         const datas = await page.evaluate(getAllP);
@@ -41,12 +37,13 @@ async function scraping(browser,lien,rech){
     }
     for(let i of Alldata){
         for(let j of i){
-            if(j.match(rech)){
-                console.log(j)
-                return j;
+            if(j.match(rech)) {
+                console.log(lda(j.match(/[^\.!\?]+[\.!\?$]+/),1,5,['fr']))
+                body+=j+"£"
             }
         }
     }
+    return body
 }
 
 function getP(){
