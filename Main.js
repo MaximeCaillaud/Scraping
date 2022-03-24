dc93c108e32df275419befc92e5f933879464bef
Marks132 <maxime.caillaud93@gmail.com>
2022-03-24 18:12:27 +0100

    Intégration du tri et de la librairie LDA

--- Main.js
+++ Main.js
@@ -1,24 +1,25 @@
 const puppeteer = require('puppeteer');
 const readline = require('readline');
+const lda = require('lda');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 rl.question("Quelle est votre recherche ?\n",rech =>{
-    getLien("https://www.google.com/search?q="+rech);
+    getLien("https://www.google.com/search?q="+rech,rech);
 });
 
-async function getLien(url) {
+async function getLien(url,rech) {
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.setViewport({height: 800, width: 1200})
     await page.goto(url);
     const lien = await page.evaluate(getAllA);
-    scraping(browser,lien)
+    scraping(browser,lien,rech)
 }
 
-async function scraping(browser,lien){
+async function scraping(browser,lien,rech){
     const page = await browser.newPage();
     let Alldata = [];
     for (let i of lien) {
@@ -26,7 +27,14 @@ async function scraping(browser,lien){
         const datas = await page.evaluate(getAllP);
         Alldata.push(datas);
     }
-    console.log(Alldata);
+    for(let i of Alldata){
+        for(let j of i){
+            if(j.match(rech)){
+                console.log(j)
+                console.log(lda(j.match(/[^\.!\?]+[\.!\?]+/g),1,3,['fr']));
+            }
+        }
+    }
 }
 
 function getP(){
